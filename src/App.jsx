ec5056fd496f82afe39e80dc6acaee0db8b5259e
Michael_J_T <lightbar200@gmail.com>
2025-12-27 01:11:55 -0600

    fixed random bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Randomizer from './components/Randomizer'
+import Randomizer from "./components/Randomizer"
 
 function App() {
   
---
+++ src/components/Randomizer.jsx
@@ -0,0 +1,10 @@
+function Randomizer() {
+    return (
+        <>
+            <h2> Drink Randomizer</h2>
+            <button> Pour it up</button>
+        </>
+    );
+}
+
+export default Randomizer

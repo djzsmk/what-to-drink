1012a5d1cfe48bf33c03eb28c60ea4152ba36c21
Michael_J_T <lightbar200@gmail.com>
2025-12-27 00:56:14 -0600

    created Randomizer comp and added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Randomizer from './components/Randomizer'
 
 function App() {
   
   return (
     <>
       <h1>shows whats on the screen</h1>
+      < Randomizer />
     </>
   )
 }

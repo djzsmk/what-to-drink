78a1d5a55b33f47b5fcbaa563c719c952e05c93f
Michael_J_T <lightbar200@gmail.com>
2025-12-27 00:46:01 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>shows whats on the screen</h1>
+    </>
+  )
+}
+
+export default App

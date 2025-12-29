4f51288b9b4bdf6dbfac85ebebd7326568c469e3
Michael_J_T <lightbar200@gmail.com>
2025-12-29 11:32:13 -0600

    prop and state test

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button ({onClick}) {
+    return <button onClick = {onClick}>Random Drink</button>
+}
+
+export default Button
--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react'
+import Button from "./Button"
+
 function Randomizer() {
+    
+    const [drink, setDrink] = useState("Click the button");
+
+    const drinks = ["Margarita", "Long Island", "Old Fashioned"];
+
+    function pickRandomDrink () {
+        const randomIndex = Math.floor(Math.random () * drinks.length)
+        setDrink(drinks[randomIndex])
+    }
+
+    
     return (
         <>
-            <h2> Drink Randomizer</h2>
-            <button> Pour it up</button>
+            <h2> {drink} </h2>
+            <Button onClick={pickRandomDrink} />
         </>
     );
-}
+};
+
+
+
 
 export default Randomizer
